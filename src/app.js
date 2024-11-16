755cbf7c39f9c0bad3c5ee09a0626185630632f5
Alejandro Quiles García <alexqg1208@gmail.com>
2024-11-16 02:42:11 +0000

    Segundo commit

--- src/app.js
+++ src/app.js
@@ -8,22 +8,16 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
 
-  let who = ["The dog", "My grandma", "The mailman", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "my phone", "the car"];
-  let when = [
-    "before the class",
-    "when I was sleeping",
-    "while I was exercising",
-    "during my lunch",
-    "while I was praying"
-  ];
+  let quien = ["El perro", "Mi abuela", "El gato"];
+  let accion = ["se comió", "se merendó", "se cenó"];
+  let que = ["mis deberes", "mi comida", "mis apuntes"];
+  let cuando = ["antes de clase", "mientras dormía", "cuando estudiaba"];
 
-  let whoRandom = who[Math.floor(Math.random() * who.length)];
-  let actionRandom = action[Math.floor(Math.random() * action.length)];
-  let whatRandom = what[Math.floor(Math.random() * what.length)];
-  let whenRandom = when[Math.floor(Math.random() * when.length)];
+  let QuienRandom = quien[Math.floor(Math.random() * quien.length)];
+  let AccionRandom = accion[Math.floor(Math.random() * accion.length)];
+  let QueRandom = que[Math.floor(Math.random() * que.length)];
+  let CuandoRandom = cuando[Math.floor(Math.random() * cuando.length)];
   console.log(
-    whoRandom + " " + actionRandom + " " + whatRandom + " " + whenRandom
+    QuienRandom + " " + AccionRandom + " " + QueRandom + " " + CuandoRandom
   );
 };
